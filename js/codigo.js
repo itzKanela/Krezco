0a0dfb2a72cb5c20240c7431284a2673ddb36f82
kanela <elenamalbitre@gmail.com>
2022-12-29 01:45:34 -0300

    ola

--- js/codigo.js
+++ js/codigo.js
@@ -1,14 +1,5 @@
-
+let panel = document.getElementById("panel");
 let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
-let btn7 = document.getElementById("btn7");
-let btn8 = document.getElementById("btn8");
-let btn9 = document.getElementById("btn9");
-let btn10 = document.getElementById("btn10");
-let btn11 = document.getElementById("btn11");
 let btn12 = document.getElementById("btn12");
 
 
@@ -30,125 +21,7 @@ document.getElementById("btn1").onclick = function (){
     redirect();
 }
 
-function redirect2(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20quiero%20convertirme%20en%20socio.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn2").onclick = function (){
-    redirect2();
-}
 
-function redirect3(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Paginas%20web%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn3").onclick = function (){
-    redirect3();
-}
-
-function redirect4(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Tienda%20on-line%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn4").onclick = function (){
-    redirect4();
-}
-
-function redirect5(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Punto%20de%20ventas%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn5").onclick = function (){
-    redirect5();
-}
-
-function redirect6(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Sitio%20de%20reservas%20on-line%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn6").onclick = function (){
-    redirect6();
-}
-
-function redirect7(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Diseño%20de%20logotipo%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn7").onclick = function (){
-    redirect7();
-}
-
-function redirect8(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Diseño%20de%20redes%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn8").onclick = function (){
-    redirect8();
-}
-
-function redirect9(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Contenido%20para%20redes%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn9").onclick = function (){
-    redirect9();
-}
-
-function redirect10(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Branding%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn10").onclick = function (){
-    redirect10();
-}
-
-function redirect11(){
-    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
-    if (confirmacion == true){
-        window.location.replace('https://api.whatsapp.com/send?phone=+34642343048&text=Hola,%20estoy%20interesado%20en%20el%20servicio%20de%20Starter%20Pack%20y%20conocer%20mas%20informaci%C3%B3n.');
-    } else {
-        window.location.reload;
-    }
-}
-document.getElementById("btn11").onclick = function (){
-    redirect11();
-}
 
 function redirect12(){
     let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
@@ -163,9 +36,17 @@ document.getElementById("btn12").onclick = function (){
 }
 
 
-
-
-
+function panel1(){
+    let confirmacion = window.confirm("Estas siendo redireccionado a otro sitio web, quieres continuar?");
+    if (confirmacion == true){
+        window.location.replace('https://krezco.net/panel/index.php/signin?redirect=https://krezco.net/panel/index.php/dashboard');
+    } else {
+        window.location.reload;
+    }
+}
+document.getElementById("panel").onclick = function (){
+    panel1();
+}
 
 
 
